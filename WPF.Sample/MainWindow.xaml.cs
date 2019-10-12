6722470388769404c37329c5f090c4d7837152eb
Johnny Cotos <jhonny_94_17@hotmail.com>
2019-10-12 08:53:02 -0500

    First commit

---
+++ WPF.Sample/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace WPF.Sample
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new Ejemplo1.App());
+        }
+    }
+}
